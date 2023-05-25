fc2fce9595ba0831b0f3d37fefdc21c3e5fbddd9
peponi <peponi_paradise@naver.com>
2023-05-25 22:04:03 +0900

    임시 자료 업로드
    ㄱㄱㄱㄱ

---
+++ DXApplication1/DXApplication1/CustomReminderForm.cs
@@ -0,0 +1,100 @@
+﻿using DevExpress.CodeParser;
+using DevExpress.Utils;
+using DevExpress.XtraScheduler;
+using DevExpress.XtraScheduler.Forms;
+using System;
+
+namespace DXApplication1
+{
+    public class CustomReminderForm : RemindersForm
+    {
+        /*-------------------------------------------
+         *
+         *      Events
+         *
+         -------------------------------------------*/
+
+        /*-------------------------------------------
+         *
+         *      Public members
+         *
+         -------------------------------------------*/
+
+        public delegate void DismissOccurredEventHandler(ReminderCollection reminders);
+
+        public event DismissOccurredEventHandler DismissOccurred;
+
+        public delegate void SnoozeOccurredEventHandler(ReminderCollection reminders, TimeSpan span);
+
+        public event SnoozeOccurredEventHandler SnoozeOccurred;
+
+        /*-------------------------------------------
+         *
+         *      Private members
+         *
+         -------------------------------------------*/
+
+        /*-------------------------------------------
+         *
+         *      Constructor / Destructor
+         *
+         -------------------------------------------*/
+
+        public CustomReminderForm(SchedulerControl schedulerControl) : base(schedulerControl)
+        {
+            //btnDismiss.Click += BtnDismiss_Click;
+            btnDismissAll.Click += BtnDismissAll_Click;
+            btnSnooze.Click += BtnSnooze_Click;
+        }
+
+        private void BtnSnooze_Click(object sender, System.EventArgs e)
+        {
+            var items = GetSelectedReminders();
+            TimeSpan span = (TimeSpan)cbSnooze.EditValue;
+            SnoozeOccurred?.Invoke(items, span);
+        }
+
+        private void BtnDismissAll_Click(object sender, System.EventArgs e)
+        {
+            var items = base.GetReminders();
+            DismissOccurred?.Invoke(items);
+        }
+
+        private void BtnDismiss_Click(object sender, System.EventArgs e)
+        {
+            var items = base.GetSelectedReminders();
+            DismissOccurred?.Invoke(items);
+        }
+
+        protected override void btnDismiss_Click(object sender, EventArgs e)
+        {
+            var items = GetSelectedReminders();
+            DismissOccurred?.Invoke(items);
+            base.btnDismiss_Click(sender, e);
+        }
+
+        /*-------------------------------------------
+         *
+         *      Event functions
+         *
+         -------------------------------------------*/
+
+        /*-------------------------------------------
+         *
+         *      Public functions
+         *
+         -------------------------------------------*/
+
+        /*-------------------------------------------
+         *
+         *      Private functions
+         *
+         -------------------------------------------*/
+
+        /*-------------------------------------------
+         *
+         *      Helper functions
+         *
+         -------------------------------------------*/
+    }
+}
---
+++ DXApplication1/DXApplication1/Form1.cs
@@ -0,0 +1,46 @@
+﻿using DevExpress.XtraScheduler;
+using DevExpress.XtraScheduler.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DXApplication1
+{
+    public partial class Form1 : DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            schedulerControl1.RemindersFormShowing += SchedulerControl1_RemindersFormShowing;
+        }
+
+        private void SchedulerControl1_RemindersFormShowing(object sender, DevExpress.XtraScheduler.RemindersFormEventArgs e)
+        {
+            CustomReminderForm form = new CustomReminderForm(schedulerControl1);
+            var args = new ReminderEventArgs(e.AlertNotifications);
+            form.SnoozeOccurred += Form_SnoozeOccurred;
+            form.DismissOccurred += Form_DismissOccurred;
+            form.OnReminderAlert(args);
+            if (form.WindowState == FormWindowState.Minimized)
+                form.WindowState = FormWindowState.Normal;
+            form.BringToFront();
+            e.Handled = true;
+        }
+
+        private void Form_DismissOccurred(DevExpress.XtraScheduler.ReminderCollection reminders)
+        {
+            Trace.WriteLine($"{reminders[0].Subject}, {reminders[0].Id}");
+        }
+
+        private void Form_SnoozeOccurred(DevExpress.XtraScheduler.ReminderCollection reminders, TimeSpan span)
+        {
+            Console.WriteLine($"{reminders[0].Subject}, {span}");
+        }
+    }
+}
